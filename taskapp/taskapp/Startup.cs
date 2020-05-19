7c062593713a6443f4a1ab719da02ef414743153
Jeff Martinez <jeffxmartinez@gmail.com>
2020-05-18 22:05:27 -0700

    adding dacpac file and sql setup

--- taskapp/taskapp/Startup.cs
+++ taskapp/taskapp/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using taskapp.Data;
 
 namespace taskapp
 {
@@ -24,6 +26,9 @@ namespace taskapp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
